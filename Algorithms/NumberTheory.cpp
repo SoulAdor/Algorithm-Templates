451af97cb2aceb86afe83fb5ee6d678532b33439
SoulAdor <niedens.a@gmail.com>
2019-09-29 23:43:56 +0400

    Functions take long long arguments

--- Algorithms/NumberTheory.cpp
+++ Algorithms/NumberTheory.cpp
@@ -1,28 +1,44 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-tuple <int, int, int> getExtendedGCD (int a, int m)
+long long mod = 1e9 + 7;
+
+// Modulo power counter
+long long ModPow (long long num, long long pow)
 {
-	if (a == 0) return {m, 0, 1};
-	auto [gcd, x, y] = getExtendedGCD (m % a, a);
-	return {gcd, y - (m / a) * x, x};
+	if (pow == 0) return 1;
+	long long mid = ModPow (num, pow / 2);
+	mid *= mid;
+	mid %= mod;
+	if (pow % 2) mid *= num;
+	return mid %= mod;
 }
 
-// With negative a
-int mod (int a, int m) 
-{ 
-    return (a % m + m) % m; 
-} 
-
-int getGCD (int a, int m)
+// Usual greatest common divisor
+long long getGCD (long long a, long long m)
 {
 	return a == 0 ? m : getGCD (m % a, a);
 }
 
-int GetInverse (int a, int m)
+// Returns modulo, works with negative a also
+long long Mod (long long a, long long m) 
+{ 
+   return (a % m + m) % m; 
+}
+
+// Extended Euclidian algorithm to get coefficients
+tuple <long long, long long, long long> ExtendedGCD (long long a, long long m)
 {
-	auto [gcd, x, y] = getExtendedGCD (a, m);  
-	return (x % m + m) % m;  			// To handle negative
+	if (a == 0) return {m, 0, 1};
+	auto [gcd, x, y] = ExtendedGCD (m % a, a);
+	return {gcd, y - (m / a) * x, x};
+}
+
+// Returns inverse of a modulo m if they are coprime
+long long GetInverse (long long a, long long m)
+{
+	auto [gcd, x, y] = ExtendedGCD (a, m);
+	return Mod(x, m);  			// To handle negative
 }
 
 int main ()
