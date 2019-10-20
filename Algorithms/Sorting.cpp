769d5dbbbe2bb7da12f93960c12bbc9506f0ca1d
SoulAdor <niedens.a@gmail.com>
2019-10-20 20:30:19 +0400

    Rewrite mergesort

--- Algorithms/Sorting.cpp
+++ Algorithms/Sorting.cpp
@@ -7,10 +7,7 @@ void MergeSort (vector <int> &v)
    vector <int> left (v.begin(), v.begin() + v.size() / 2); MergeSort (left);
    vector <int> right (v.begin() + v.size() / 2, v.end());  MergeSort (right);
    for (int i = 0, l = 0, r = 0; i < v.size(); i++)
-      v[i] = 
-         r == right.size() ? left[l++] : 
-         l == left.size() ? right[r++] : 
-         left[l] < right[r] ? left[l++] : right[r++];
+      v[i] = r == right.size() || (l != left.size() && left[l] < right[r]) ? left[l++] : right[r++];
 }
 
 void InsertionSort (vector <int>& v)
