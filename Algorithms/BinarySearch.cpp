1c8c7070cc7c383ca89f7b72eacd919ba3046267
SoulAdor <niedens.a@gmail.com>
2020-02-19 17:32:17 +0400

    Add Peak finds

--- Algorithms/BinarySearch.cpp
+++ Algorithms/BinarySearch.cpp
@@ -1,47 +1,56 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-// Might loop on case start + 1 = end, if 1 not added properly in (end - start) / 2
-
 vector <int> v {1, 1, 3, 3, 3, 5, 5};
 
 // Returns id of searched element in sorted list, -1 if not found. 
-int BinarySearch (int start, int finish, int value)
+long long BinarySearch (long long start, long long finish, long long value)
 {
-   int low = 0, mid, high = 3;
-   while (low <= high)
+   while (start < finish)
    {
-      mid = low + (high - low) / 2;
+      long long mid = start + (finish - start) / 2;
       if (v[mid] == value) return mid;
-      v[mid] < value ? low = mid + 1 : high = mid - 1;
+      v[mid] < value ? start = mid + 1 : finish = mid - 1;
    }
    return -1;
 }
 
-bool Check (int id)
+bool Check (long long value)
 {
-   return v[id] >= 2;
+   return v[value] >= 3;
 }
 
-int PeakFind (int start, int finish)
+long long FirstTrue (long long start, long long finish)
 {
-   int low = 0, mid, high = 3;
-   while (low != high)
+   while (start != finish)
    {
-      mid = low + (high - low) / 2;
-      Check (mid) ? high = mid : low = mid + 1;
+      long long mid = start + (finish - start) / 2;
+      Check (mid) ? finish = mid : start = mid + 1;
    }
-   return low;
+   return start;
+}
+
+long long LastFalse (long long start, long long finish)
+{
+   while (start != finish)
+   {
+      long long mid = start + (finish - start + 1) / 2;
+      Check (mid) ? finish = mid - 1 : start = mid;
+   }
+   return start;
 }
 
 int main()
 { 
-   // lower_bound returns a pointer to the first array element whose value is at least x.
-   // upper_bound returns a pointer to the first array element whose value is larger than x.
+   // lower_bound returns a pointer to the first array element whose value is at least x, or end().
+   // upper_bound returns a pointer to the first array element whose value is larger than x, or end().
    // equal_range returns both above pointers.
    // If there is no such element, the pointer points to the element after the last array element.
    cout <<  lower_bound (v.begin(), v.end(), 3) - v.begin() << endl;
    cout <<  upper_bound (v.begin(), v.end(), 3) - v.begin() << endl;
    auto r = equal_range(v.begin(), v.end(), 3);
    cout << "Number of elements: " << r.second - r.first << "\n";
+   cout << BinarySearch (0,5,3) << "\n";
+   cout << FirstTrue (0,5) << "\n";
+   cout << LastFalse (0,5) << "\n";
 }
